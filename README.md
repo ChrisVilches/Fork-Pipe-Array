dd5af7fee5cc206b546d8246383b7d396cfa7c71
FeloVilches <felovilches@gmail.com>
2015-11-24 08:31:14 -0300

    Cambie el readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Fork-Pipe-Array
+# Fork, Pipe & Array
+
+Programa que de manera binarizada envia un arreglo desde un proceso a otro (desde el padre al hijo).
+
+El proceso hijo lo modifica, y lo envia de vuelta al proceso padre.
+
+En vez de enviar strings, es mejor enviarlo binarizado, para que no sea necesario cortar, eliminar saltos de linea, o usar atoi.
+
+Creado para implementar el modulo de lectura y escritura por pipe del lab 1 de sistemas operativos.
+
+Por Felipe Vilches C.
---
+++ main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+#define LECTURA 0
+#define ESCRITURA 1
+
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+    // Pipes
+    int padreAhijo[2];
+    int hijoApadre[2];
+    pipe(padreAhijo);
+    pipe(hijoApadre);
+
+    // Fork
+    pid_t pid;
+    pid = fork();
+
+    if(pid == 0){
+
+        int tamanoArreglo;
+        int* arreglo;
+        int i;
+
+        // Hijo
+        close(padreAhijo[ESCRITURA]);   // No se necesita
+        close(hijoApadre[LECTURA]);     // No se necesita
+
+        // Leer la dimension
+        read(padreAhijo[LECTURA], &tamanoArreglo, sizeof(int)); // Se lee un int
+
+        // Malloc arreglo
+        arreglo = (int*) malloc(sizeof(int) * tamanoArreglo);
+
+        // Comenzar a leer los datos del arreglo
+        for(i=0; i<tamanoArreglo; i++){
+            // Lo guardo inmediatamente en el arreglo (cada read lee un int del pipe)
+            // (Aunque se puede guardar en un int temporal)
+            read(padreAhijo[LECTURA], &arreglo[i], sizeof(int));
+        }
+
+        close(padreAhijo[LECTURA]);     // Ya no se necesita
+
+        // Modificamos el arreglo (podria haberse hecho en el mismo for de la lectura)
+        for(i=0; i<tamanoArreglo; i++){
+            arreglo[i] += i*2 + 1;      // Modificacion arbitraria
+        }
+
+        // Escribir el arreglo modificado por el pipe hacia el padre
+        for(i=0; i<tamanoArreglo; i++){
+            // cada write escribe un int hacia el pipe
+            write(hijoApadre[ESCRITURA], &arreglo[i], sizeof(int));
+        }
+
+        close(hijoApadre[ESCRITURA]);   // Ya no se necesita
+
+        exit(0);
+
+    }
+
+    // Padre
+
+    int tamanoArreglo = 10;                     // Tamano del arreglo
+    int arreglo[] = {4,5,7,7,4,5,7,8,9,0};      // Crear el arreglo
+    int i;
+
+    close(padreAhijo[LECTURA]);                 // No se necesitan
+    close(hijoApadre[ESCRITURA]);
+
+    // Primero escribimos la dimension del arreglo
+    write(padreAhijo[ESCRITURA], &tamanoArreglo, sizeof(int));
+
+    // Escribir los int al pipe
+    for(i=0; i<tamanoArreglo; i++){
+        write(padreAhijo[ESCRITURA], &arreglo[i], sizeof(int));
+    }
+
+    close(padreAhijo[ESCRITURA]);               // Ya termino de escribir, no se usara
+
+    // Leer los datos que el hijo modifico
+    for(i=0; i<tamanoArreglo; i++){
+        // Los guardo en el arreglo original, reemplazando los valores
+        read(hijoApadre[LECTURA], &arreglo[i], sizeof(int));
+    }
+
+    close(hijoApadre[LECTURA]);                 // Ya termino de leer, hay que cerrarlo
+
+    // Imprimir
+    for(i=0; i<tamanoArreglo; i++){
+        cout << "Arreglo[" << i << "] = " << arreglo[i] << endl;
+    }
+
+
+    return 0;
+}
